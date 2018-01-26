abdb3d7aa314ed7eb0aace8005e916b17386617c
Unknown <madmazoku@gmail.com>
2018-01-26 08:22:17 -0800

    reformat

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,8 @@ int main(int argc, char** argv)
 {
     boost::program_options::options_description desc("Allowed options");
     desc.add_options()
-        ("help,h", "print usage message")
-        ("version,v", "print version number");
+    ("help,h", "print usage message")
+    ("version,v", "print version number");
 
     boost::program_options::variables_map vm;
     boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
@@ -29,12 +29,10 @@ int main(int argc, char** argv)
 
     if (vm.count("help")) {
         std::cout << desc << std::endl;
-    }
-    else if (vm.count("version")) {
+    } else if (vm.count("version")) {
         std::cout << "Build version: " << build_version() << std::endl;
         std::cout << "Boost version: " << (BOOST_VERSION / 100000) << '.' << (BOOST_VERSION / 100 % 1000) << '.' << (BOOST_VERSION % 100) << std::endl;
-    }
-    else {
+    } else {
 
         do_matrix();
 
--- src/sparse_matrix.h
+++ src/sparse_matrix.h
@@ -6,78 +6,78 @@
 namespace sm_helpers
 {
 
-    template<size_t dim, typename... Is>
-    struct indexer {
-        static auto index(Is... I)
-        {
-            return indexer<dim - 1, size_t, Is...>::index(dim - 1, I...);
-        }
-    };
+template<size_t dim, typename... Is>
+struct indexer {
+    static auto index(Is... I)
+    {
+        return indexer<dim - 1, size_t, Is...>::index(dim - 1, I...);
+    }
+};
 
-    template<typename... Is>
-    struct indexer<0, Is...> {
-        static auto index(Is... I)
-        {
-            return std::make_tuple(I...);
-        }
-    };
+template<typename... Is>
+struct indexer<0, Is...> {
+    static auto index(Is... I)
+    {
+        return std::make_tuple(I...);
+    }
+};
 
-    template<size_t dim>
-    using loc_type = decltype(indexer<dim>::index());
+template<size_t dim>
+using loc_type = decltype(indexer<dim>::index());
 
-    template<size_t dim, typename T>
-    using map_type = std::map< sm_helpers::loc_type<dim>, T >;
+template<size_t dim, typename T>
+using map_type = std::map< sm_helpers::loc_type<dim>, T >;
 
-    template<size_t u_dim, size_t dim, typename T, T def_val>
-    struct loc_value {
-    public:
-        using map_type = sm_helpers::map_type<dim, T>;
-        using loc_type = sm_helpers::loc_type<dim - u_dim>;
-        using next_loc_value = loc_value<u_dim - 1, dim, T, def_val>;
-        map_type& _matrix;
-        loc_type _loc;
+template<size_t u_dim, size_t dim, typename T, T def_val>
+struct loc_value {
+public:
+    using map_type = sm_helpers::map_type<dim, T>;
+    using loc_type = sm_helpers::loc_type<dim - u_dim>;
+    using next_loc_value = loc_value<u_dim - 1, dim, T, def_val>;
+    map_type& _matrix;
+    loc_type _loc;
 
-        loc_value(map_type& matrix, loc_type loc) : _matrix(matrix), _loc(loc) {}
+    loc_value(map_type& matrix, loc_type loc) : _matrix(matrix), _loc(loc) {}
 
-        next_loc_value operator[](size_t n)
-        {
-            return next_loc_value{ _matrix, std::tuple_cat(_loc, std::make_tuple(n)) };
-        }
-    };
+    next_loc_value operator[](size_t n)
+    {
+        return next_loc_value{ _matrix, std::tuple_cat(_loc, std::make_tuple(n)) };
+    }
+};
 
-    template<size_t dim, typename T, T def_val>
-    struct loc_value<0, dim, T, def_val> {
-    public:
-        using map_type = sm_helpers::map_type<dim, T>;
-        using loc_type = sm_helpers::loc_type<dim>;
-        using last_loc_value = loc_value<0, dim, T, def_val>;
-        map_type& _matrix;
-        loc_type _loc;
+template<size_t dim, typename T, T def_val>
+struct loc_value<0, dim, T, def_val> {
+public:
+    using map_type = sm_helpers::map_type<dim, T>;
+    using loc_type = sm_helpers::loc_type<dim>;
+    using last_loc_value = loc_value<0, dim, T, def_val>;
+    map_type& _matrix;
+    loc_type _loc;
 
-        loc_value(map_type& matrix, loc_type loc) : _matrix(matrix), _loc(loc) {}
+    loc_value(map_type& matrix, loc_type loc) : _matrix(matrix), _loc(loc) {}
 
-        last_loc_value& operator=(const T& t)
-        {
-            auto f = _matrix.find(_loc);
-            if (t != def_val)
-                if (f == _matrix.end())
-                    _matrix.emplace(std::make_pair(_loc, t));
-                else
-                    f->second = t;
-            else if (f != _matrix.end())
-                _matrix.erase(f);
-            return *this;
-        }
-
-        operator T() const
-        {
-            auto f = _matrix.find(_loc);
+    last_loc_value& operator=(const T& t)
+    {
+        auto f = _matrix.find(_loc);
+        if (t != def_val)
             if (f == _matrix.end())
-                return def_val;
+                _matrix.emplace(std::make_pair(_loc, t));
             else
-                return f->second;
-        }
-    };
+                f->second = t;
+        else if (f != _matrix.end())
+            _matrix.erase(f);
+        return *this;
+    }
+
+    operator T() const
+    {
+        auto f = _matrix.find(_loc);
+        if (f == _matrix.end())
+            return def_val;
+        else
+            return f->second;
+    }
+};
 
 }
 
