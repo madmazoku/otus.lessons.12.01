845572f56b8d30cdcf3bd16e39758f3ad601d0ec
Moskalyov Dmitry <madmazoku@gmail.com>
2018-01-26 19:21:22 +0300

    reformat

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,8 @@ int main(int argc, char** argv)
 {
     boost::program_options::options_description desc("Allowed options");
     desc.add_options()
-    ("help,h", "print usage message")
-    ("version,v", "print version number");
+        ("help,h", "print usage message")
+        ("version,v", "print version number");
 
     boost::program_options::variables_map vm;
     boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
@@ -29,10 +29,12 @@ int main(int argc, char** argv)
 
     if (vm.count("help")) {
         std::cout << desc << std::endl;
-    } else if (vm.count("version")) {
+    }
+    else if (vm.count("version")) {
         std::cout << "Build version: " << build_version() << std::endl;
         std::cout << "Boost version: " << (BOOST_VERSION / 100000) << '.' << (BOOST_VERSION / 100 % 1000) << '.' << (BOOST_VERSION % 100) << std::endl;
-    } else {
+    }
+    else {
 
         do_matrix();
 
@@ -47,15 +49,15 @@ int main(int argc, char** argv)
 
 void do_matrix()
 {
-    sparse_matrix<2,int, 0> sm;
-    for(size_t n = 0; n <= 9; ++n) {
+    sparse_matrix<2, int, 0> sm;
+    for (size_t n = 0; n <= 9; ++n) {
         sm[n][n] = n;
-        sm[n][9-n] = 9-n;
+        sm[n][9 - n] = 9 - n;
     }
 
-    for(size_t y = 1; y <= 8; ++y) {
-        for(size_t x = 1; x <= 8; ++x) {
-            if(x != 1)
+    for (size_t y = 1; y <= 8; ++y) {
+        for (size_t x = 1; x <= 8; ++x) {
+            if (x != 1)
                 std::cout << ' ';
             std::cout << sm[x][y];
         }
@@ -66,7 +68,7 @@ void do_matrix()
     std::cout << "matrix size: " << sm.size() << std::endl;
     std::cout << std::endl;
 
-    for(auto e : sm) {
+    for (auto e : sm) {
         size_t x, y;
         std::tie(x, y) = e.first;
         std::cout << "matrix[" << x << ", " << y << "] = " << e.second << std::endl;
